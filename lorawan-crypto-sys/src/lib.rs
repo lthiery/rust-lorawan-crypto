906bd007fcf9f7000d7b74b2379f79efbf2c4e31
John Greene <jgreene@volleyboast.com>
2022-10-06 11:08:28 -0500

    Building successfully with cross-rs (#1)

--- lorawan-crypto-sys/src/lib.rs
+++ lorawan-crypto-sys/src/lib.rs
@@ -4,4 +4,15 @@
 #![allow(clippy::all)]
 #![cfg_attr(not(test), no_std)]
 
+#![allow(unused)]
+#![no_main]
+#![no_std]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
+}
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
